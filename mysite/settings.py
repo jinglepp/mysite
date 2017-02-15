88c4beabfbb6a2d6062646218d920c9936491936
jinglepp <jinglepp@qq.com>
2017-02-16 01:00:47 +0800

    更新polls的models

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls",
-    "news",
+    "polls.apps.PollsConfig",
+    "news.apps.NewsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
